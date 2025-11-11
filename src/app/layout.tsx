d3530669b2f73e9700f125b081e8a0d2306f1e9e
Harris shoukat <harrisshoukat06@gmail.com>
2025-11-11 21:46:42 +0500

    uncomment splash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* <SplashScreen /> */}
+        <SplashScreen />
         <Navbar />
         {children}
         <Footer />
